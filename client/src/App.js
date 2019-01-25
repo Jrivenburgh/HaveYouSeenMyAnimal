8efa191e3f1fe613a18748cb82fd09c768b865e0
JRivenburgh <jurassicjosh429@gmail.com>
2019-01-24 21:36:38 -0800

    in class 24

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Animals from "./pages/Animals";
+import Detail from "./pages/Detail";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Animals} />
+          <Route exact path="/Animals" component={Animals} />
+          <Route exact path="/Animals/:id" component={Detail} />
+          <Route component={NoMatch} />
+          <Route exact path="/Animals/Login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
