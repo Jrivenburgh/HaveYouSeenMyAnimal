24fbdd1a6a19f51566312cd748afd459a6d23a80
loeljee <loeljee@gmail.com>
2019-01-31 19:08:44 -0800

    nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,7 +3,7 @@ import "./Nav.css";
 const Nav = props => (
   <div className="header">
       <ul>
-          <li><h2 className="title animated swing">LookOut4 My Animal</h2></li>
+          <li><a href="/"><h2 className="title animated swing">LookOut4 My Animal</h2></a></li>
           {/* <GameMessage score={props.score} topScore={props.topScore} /> */}
           {/* <li id="score">Score: {props.score} | Top Score: {props.topScore}</li> */}
           <li className="log"><a href="/Login"><span><img className='login' alt-text= "login button"/></span> Log In</a> | <a href="/Signup"><span><img className='signup' alt-text= "signup button"/></span> Sign Up</a></li>
