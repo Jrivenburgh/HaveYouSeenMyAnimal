5b6cf2670f0f3e4180499e90797db67015ad8aa4
JRivenburgh <jurassicjosh429@gmail.com>
2019-02-12 16:54:32 -0800

    host issues

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const config = require('./db');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const users = require('./routes/user'); 
 const path = require('path');
 
